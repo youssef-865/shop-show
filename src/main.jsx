90cbb25283b5863a26db5ca410ebedb83f933fa8
youssef mohy <youssefmohy865@gmail.com>
2025-03-20 15:09:38 +0200

    Added public/index.html for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import App from './App.jsx';
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css"; // ✅ المسار الصحيح
 
-// Disable console in production
 if (import.meta.env.PROD) {
   window.console.log = () => {};
   window.console.warn = () => {};
@@ -15,7 +15,6 @@ if (import.meta.env.PROD) {
   window.console.debug = () => {};
 }
 
-// Prevent opening DevTools
 document.addEventListener("keydown", function (event) {
   if (
     event.key === "F12" ||
@@ -27,10 +26,7 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
-// Disable right-click
 // document.addEventListener("contextmenu", (event) => event.preventDefault());
-
-import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -39,4 +35,3 @@ createRoot(document.getElementById('root')).render(
     </HashRouter>
   </StrictMode>,
 );
-
