2df862393f93805b66125b9f4f2e9cd898b40534
youssef mohy <youssefmohy865@gmail.com>
2025-03-20 15:07:53 +0200

    Added public/index.html for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import App from './App.jsx';
-import "../node_modules/@fortawesome/fontawesome-free";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 // Disable console in production
 if (import.meta.env.PROD) {
@@ -30,8 +30,13 @@ document.addEventListener("keydown", function (event) {
 // Disable right-click
 // document.addEventListener("contextmenu", (event) => event.preventDefault());
 
+import { HashRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 );
+
